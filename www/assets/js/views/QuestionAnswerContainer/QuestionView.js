d22e4601310bf09f1b9d3dec891d8e3235428046
Tim Black <tim@alwaysreformed.com>
2014-05-07 07:46:53 -0500

    Get nav links to work

--- www/assets/js/views/QuestionAnswerContainer/QuestionView.js
+++ www/assets/js/views/QuestionAnswerContainer/QuestionView.js
@@ -10,13 +10,18 @@ define(
     return Backbone.View.extend({
         initialize: function(){
             this.render()
+            _.bindAll(this, 'give_answer')
         },
         render: function(){
             $(this.el).html(Mustache.render(template, this.model.attributes));
-            this.listenTo(this.$('#true, #false'), 'click', this.give_answer)
+            // TODO: This doesn't seem to attach the event handler correctly
+            // Start here.
+            this.listenTo(this.$('#true'), 'click', this.give_answer)
+            this.listenTo(this.$('#false'), 'click', this.give_answer)
         },
         give_answer:function(event){
             // TODO: Display scripture answers
+            this.$('.answer').show()
             // TODO: Play sound and provide overlay for whether they got it right or not.
             //  E.g., thunder and lightning, or a glow and the sound of angels singing.
             //  A variety of such good and bad sounds/overlays would be fun.
