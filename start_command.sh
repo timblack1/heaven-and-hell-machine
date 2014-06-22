745fdfd9756b8b3f21fe82753749167b9a5791f7
Tim Black <tim@alwaysreformed.com>
2014-06-22 03:19:46 +0000

    Don't use HOODIE_BIND_ADDRESS or COUCH_URL

--- start_command.sh
+++ start_command.sh
@@ -1,3 +1,3 @@
-export HOODIE_BIND_ADDRESS=0.0.0.0:11400
-export COUCH_URL=http://arwd.iriscouch.com/
-hoodie start -n --custom_ports 26029,11400
+#export HOODIE_BIND_ADDRESS=0.0.0.0:11411
+#export COUCH_URL=http://127.0.0.1:11411
+hoodie start -n --custom-ports 11411,26029,11400
